7f8879751a0a1b4d8c1112e142fbd5f0455967e9
kivtor <112388316+kivtorsm@users.noreply.github.com>
2023-09-25 10:07:58 +0200

    refactorized views

---
+++ profiles/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from profiles import views
+
+app_name = "profiles"
+
+urlpatterns = [
+    path('profiles/', views.index, name='index'),
+    path('profiles/<str:username>/', views.profile, name='profile'),
+]
