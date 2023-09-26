548543df4256506e4632982a9d7491420859b313
kivtor <serradilla.victor@gmail.com>
2023-09-26 16:47:52 +0200

    added docstrings

--- manage.py
+++ manage.py
@@ -3,6 +3,10 @@ import sys
 
 
 def main():
+    """
+    Main function launching app server
+    :return: None
+    """
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oc_lettings_site.settings')
     try:
         from django.core.management import execute_from_command_line
