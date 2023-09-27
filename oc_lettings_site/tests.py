c5a69d89f005774213343b4972fc70092422fbd9
kivtor <serradilla.victor@gmail.com>
2023-09-27 11:37:30 +0200

    added url test

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,8 +1,16 @@
+from django.urls import reverse, resolve
 
-def test_should_return_homepage(client):
+
+def test_homepage_view(client):
     response = client.get('/')
     assert response.status_code == 200
     assert "Welcome to Holiday Homes" in response.content.decode()
+
+
+def test_homepage_url(client):
+    path = reverse('index')
+    assert path == '/'
+    assert resolve(path).view_name == 'index'
 
 
 def test_should_return_admin_page(admin_client):
