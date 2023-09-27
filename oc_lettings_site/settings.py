9a3254bc257e4854bf74871855b8c318d1e76ac7
kivtor <serradilla.victor@gmail.com>
2023-09-27 10:24:26 +0200

    default auto field setting for model ids

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -114,3 +114,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static", ]
+
+
+# Default model ID
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
