fa1a06efd9351433f6cecc3136fec9a8255a4bcb
kivtor <serradilla.victor@gmail.com>
2023-09-27 10:47:23 +0200

    main page tests

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,10 @@
-def test_dummy():
-    assert 1
+
+def test_should_return_homepage(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert "Welcome to Holiday Homes" in response.content.decode()
+
+
+def test_should_return_admin_page(admin_client):
+    response = admin_client.get('/admin/')
+    assert response.status_code == 200
