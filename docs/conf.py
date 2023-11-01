9a1288692125065fd5419470a48012c6fd45df53
Victor Serradilla <serradilla.victor@gmail.com>
2023-11-01 13:17:45 +0100

    no django import

--- docs/conf.py
+++ docs/conf.py
@@ -5,13 +5,11 @@
 
 import os
 import sys
-import django
 
 
 sys.path.insert(0, os.path.abspath('..'))
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'oc_lettings_site.settings'
-django.setup()
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
